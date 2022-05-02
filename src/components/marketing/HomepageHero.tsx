63a00a212325c19451e2b1c56aaf33ce28fc3135
Nick Bordeau <4759596+Borduhh@users.noreply.github.com>
2022-05-02 11:52:04 -0400

    feat: updated tagline

--- src/components/marketing/HomepageHero.tsx
+++ src/components/marketing/HomepageHero.tsx
@@ -23,9 +23,9 @@ const HomepageHero: React.FC<Props> = ({ onClick }) => (
             <img src={Logo} alt="Green Action Studio" />
           </a>
           <h1 className="pt-6 text-2xl font-extrabold tracking-tight text-gray-100 sm:mt-5 lg:mt-6 lg:text-4xl">
-            <span className="block">EV charging software</span>
+            <span className="block">The EV Charging</span>
             <span className="block text-4xl text-primary-500 lg:text-6xl">
-              That Changes The World
+              Software Solution
             </span>
           </h1>
           <p className="mt-3 text-base text-gray-100 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
